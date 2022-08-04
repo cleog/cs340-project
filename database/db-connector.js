f03c47c4009dd0618aca2e68f76b096cee070ab4
Alvin Chan <chanalv@flip2.engr.oregonstate.edu>
2022-08-04 04:35:36 -0700

    added dateStrings as connection option to solve date appearance issue

--- database/db-connector.js
+++ database/db-connector.js
@@ -5,6 +5,15 @@
 //     Source Type: source code and information guide
 //     Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 //     Date Accessed: 7/25/2022
+//
+//     The following document was referenced for the dateString connection option:
+//     Source Title: mysql
+//     Author(s): Please find a list of the contributors to this information reference guide
+//                at https://github.com/mysqljs/mysql
+//     Source Type: information reference guide
+//     Source URL: https://www.npmjs.com/package/mysql#connection-options
+//     Date Accessed: 8/4/2022
+
 
 // ./database/db-connector.js
 
@@ -17,7 +26,8 @@ var pool = mysql.createPool({
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_goldsc',
     password        : '0479',
-    database        : 'cs340_goldsc'
+    database        : 'cs340_goldsc',
+    dateStrings     : true
 })
 
 // Export it for use in our applicaiton
