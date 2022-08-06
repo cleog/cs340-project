242ee3aa3372c07436f5811b9bf470e2ca67b6d0
Cleo Golds <goldsc@flip1.engr.oregonstate.edu>
2022-08-06 14:30:29 -0700

    fixed update dropdown

--- public/js/add_book.js
+++ public/js/add_book.js
@@ -163,7 +163,7 @@ addRowToTable = (data) => {
     // // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh
     let selectMenu = document.getElementById("input-book_id-update");
     let option = document.createElement("option");
-    option.text = newRow.book_id; 
+    option.text = newRow.book_id + ': ' + newRow.title_name; 
     option.value = newRow.book_id; 
     selectMenu.add(option);
 }
